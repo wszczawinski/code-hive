08e13012e6a09e976e968951eac2b06b425c7bcc
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2023-12-02 22:15:37 +0100

    feat(read_key function + flush screen)

--- src/editor.rs
+++ src/editor.rs
@@ -1,36 +1,64 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 fn handle_err(e: std::io::Error) {
-    panic!("{}", e);
+    panic!("{}{}", termion::clear::All, e);
+}
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
 }
 
 impl Editor {
-    pub fn start(&self) {
+    pub fn default() -> Self {
+        Self { should_quit: false }
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('c') => self.should_quit = true,
+            _ => (),
+        }
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        print!(
+            "{}{}{}",
+            termion::clear::All,
+            termion::cursor::Goto(1, 1),
+            termion::cursor::BlinkingUnderline
+        );
+        if self.should_quit {
+            println!("Bye!\r");
+        }
+        io::stdout().flush()
+    }
+
+    pub fn start(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} -> {}\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('c') => break,
-                    _ => println!("{:?}\r", key),
-                },
-                Err(err) => handle_err(err),
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                handle_err(error);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                handle_err(error);
             }
         }
-    }
-    pub fn default() -> Self {
-        Editor {}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+#![warn(clippy::all, clippy::pedantic)]
 mod editor;
 use editor::Editor;
 
 fn main() {
-    let editor = Editor::default();
-    editor.start();
+    Editor::default().start();
 }
